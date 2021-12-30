e3cfc96bef3ae2adf87bd4101b1566804c4fc63f
pavannagre3 <90178716+pavannagre3@users.noreply.github.com>
2021-12-30 15:23:00 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -15,18 +15,22 @@ function Header({ getData }) {
         let result = getData.filter((el) => { if (el.Title.toUpperCase().includes(`${movie.toUpperCase()}`) && (movie !== "")) { return el } else { return ""; } });
         setSearchArr(result);
     }
+
     function byIMDb() {
         const resImdb = getData.sort(function (a, b) { return b.imdbRating - a.imdbRating; });
-        setSortedData(resImdb);
+        setSortedData([...resImdb]);
     }
+
     function byBoxOffice() {
         const resMetaScore = getData.sort(function (a, b) { return b.Metascore - a.Metascore; });
-        setSortedData(resMetaScore);
+        setSortedData([...resMetaScore]);
     }
+
     function byYear() {
         const resYear = getData.sort(function (a, b) { return b.Year - a.Year; });
-        setSortedData(resYear);
+        setSortedData([...resYear]);
     }
+
     function boolfn() {
         if (movie !== "") {
             setBool(true);
@@ -35,6 +39,7 @@ function Header({ getData }) {
             setBool(false);
         }
     }
+
     useEffect(() => {
         console.log("in useeffect")
         searchFunction();
@@ -59,8 +64,12 @@ function Header({ getData }) {
                     <button onClick={() => { byYear() }} >Sort By Year</button>
                 </div>
             </div>
-            <div style={{ display: "flex", flexWrap: "wrap" }}>
-                {bool ? <Movies getData={searchArr} /> : <Movies getData={sortedData} />}
+            <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+                {bool
+                    ?
+                    <>{searchArr.length ? <Movies getData={searchArr} /> : <h1>Sorry ! No movie found. 😐</h1>} </>
+
+                    : <Movies getData={sortedData} />}
             </div>
         </>
     );
